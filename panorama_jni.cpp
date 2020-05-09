f7d6be0ad4ed43dba8f1b1150324d6adc5a32dc3
白颢 <hao.bai@traxretail.com>
2020-05-09 11:48:26 +0800

    写个jni

---
+++ panorama_jni.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Hao Bai on 2020/5/9.
+//
+
+#include "panorama_jni.h"
---
+++ panorama_jni.h
@@ -0,0 +1,8 @@
+//
+// Created by Hao Bai on 2020/5/9.
+//
+
+#ifndef PANORAMA_PANORAMA_JNI_H
+#define PANORAMA_PANORAMA_JNI_H
+
+#endif //PANORAMA_PANORAMA_JNI_H
